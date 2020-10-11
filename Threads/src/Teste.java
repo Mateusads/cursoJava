3843f09b367f974269c454344d7ef1737bf8ca97
Mateus <mateus-medeiross@hotmail.com>
2020-10-10 22:51:11 -0300

    Aula Threds

---
+++ Threads/src/Teste.java
@@ -0,0 +1,21 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 500);
+		
+		MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 700);
+		
+		MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 900);
+		
+		MinhaThreadRunnable thread4 = new MinhaThreadRunnable("#4", 1100);
+		
+		MinhaThreadRunnable thread5 = new MinhaThreadRunnable("#5", 1500);
+		
+		MinhaThreadRunnable thread6 = new MinhaThreadRunnable("#6", 2500);
+
+
+	}
+
+}
